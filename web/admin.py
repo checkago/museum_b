912763098c076851de1beddc92cf3d833d32b840
Eugene Checkoy <mfalcon@mail.ru>
2022-06-01 10:35:51 +0300

    simfonii

--- web/admin.py
+++ web/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from web.models import *
+from django import forms
+from ckeditor.widgets import CKEditorWidget
 
-admin.site.register(Simphony,)
+
+class SimphonyAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget(config_name='awesome_ckeditor'))
+
+    class Meta:
+        verbose_name = 'Текст'
+        model = Simphony
+        fields = '__all__'
+
+
+class SimphonyAdmin(admin.ModelAdmin):
+    form = SimphonyAdminForm
+    list_display = ('title',)
+
+
+admin.site.register(Simphony, SimphonyAdmin)
